87475f12dee4defd5112a265c640a8c31dd7e420
JIN26 <Angelmantilla26@gmail.com>
2020-01-19 21:27:33 -0500

    listo

--- src/clases/MiListener.java
+++ src/clases/MiListener.java
@@ -4,7 +4,13 @@
  * and open the template in the editor.
  */
 package clases;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
 /**
  *
  * @author PC-005
@@ -18,14 +24,59 @@ public class MiListener implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e){
         if(e.getSource() == window.getSave()){
-            System.out.println("Guardar");
-                return;
+        	JDialog dialogo = new JDialog();
+        	final JLabel label = new JLabel("Guardado");
+        	dialogo.setSize(200, 100);
+    		dialogo.setLocation(250, 300);
+        	dialogo.getContentPane().add(label);
+        	dialogo.setVisible(true);
+            
+            window.getText1().setText(null);;
+            window.getText2().setText(null);
+            window.getJComboBox().setSelectedIndex(0);
+            window.getText3().setText(null);
+            return;
         }
         if(e.getSource() == window.getCancel()){
             System.out.println("Cancelar");
             return;
         }
-        //System.out.println("Aceptar2");
+        if(e.getSource() == window.getNegrita()) {
+        	if(window.getValB()==true) {
+        		window.setValB(false);
+        		window.getText4().setFont(new Font(window.getText4().getFont().getFontName(),window.getText4().getFont().getStyle()-1,window.getText4().getFont().getSize()));
+        	}else {
+        		window.setValB(true);
+        		window.getText4().setFont(new Font(window.getText4().getFont().getFontName(),window.getText4().getFont().getStyle()+1,window.getText4().getFont().getSize()));
+        	}
+        
+        	
+        }
+        if(e.getSource() == window.getCursiva()) {
+        	if(window.getValI()==true) {
+        		window.setValI(false);
+        		window.getText4().setFont(new Font(window.getText4().getFont().getFontName(),window.getText4().getFont().getStyle()-2,window.getText4().getFont().getSize()));
+        	}else {
+        		window.setValI(true);
+        		window.getText4().setFont(new Font(window.getText4().getFont().getFontName(),window.getText4().getFont().getStyle()+2,window.getText4().getFont().getSize()));
+        	}
+        }
+        if(e.getSource() == window.getColor()) {
+       
+        	 Color c = JColorChooser.showDialog(null, "Choose a Color", window.getText4().getForeground());
+             if (c != null)
+            	 window.getText4().setForeground(c);
+        }
+        if(e.getSource()==window.getJComboBoxT()) {
+        	window.getText4().setFont(new Font(window.getJComboBoxT().getSelectedItem().toString(),window.getText4().getFont().getStyle(),window.getText4().getFont().getSize()));
+        }
+       
+        if(e.getSource()==window.getJComboBoxTam()) {
+
+        	int i =Integer.valueOf(window.getJComboBoxTam().getSelectedItem().toString());
+        	
+        	window.getText4().setFont(new Font(window.getText4().getFont().getFontName(),window.getText4().getFont().getStyle(),i));
+        }
     }
     
 }
--- src/clases/Ventana3.java
+++ src/clases/Ventana3.java
@@ -11,7 +11,16 @@ import javax.swing.*;
  *
  */
 public class Ventana3 extends JFrame{
-    private JPanel content1;
+	private JPanel content0; 
+	private JPanel content1;
+	private JPanel textEdit; 
+    private JPanel panel1;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel pnlNorte; 
+    private JPanel pnlCentro;
+    private JPanel pnlSur; 
     private JButton save;
     private JButton cancel;
     private JButton negrita;
@@ -19,6 +28,16 @@ public class Ventana3 extends JFrame{
     private JButton color;
     private JButton tamanio;
     private JButton tipo;
+    private JTextField text1;
+    private JTextField text2;
+    private JTextArea text3;
+    private JTextArea text4;
+    private JComboBox jComboBox;
+    private JComboBox jComboBoxT;
+    private JComboBox jComboBoxTam;
+    private boolean valB=false;
+    private boolean valI=false;
+    
 	/**
 	 * 
 	 */
@@ -26,94 +45,136 @@ public class Ventana3 extends JFrame{
             // TODO Auto-generated constructor stub
             super("Mis datos");
 		
-            this.setSize(400,400);
-            this.setMinimumSize(new Dimension(350, 350));
+            this.setSize(415,440);
             this.setLocation(100,100);
+            setResizable(false);  
             content1 = new JPanel(new BorderLayout());
+            content1.setOpaque(true);
             setContentPane(content1);
             this.setVisible(true);
 		
-            JPanel panel1 = new JPanel();
-            JTextField text1 = new JTextField();
-            text1.setColumns(5);
+            panel1 = new JPanel();
+            text1 = new JTextField();
+            text1.setColumns(20);
             panel1.add(new JLabel("Nombre: "));
             panel1.add(text1);
 		
-            JPanel panel2 = new JPanel();
-            JTextField text2 = new JTextField();
-            text2.setColumns(5);
+            panel2 = new JPanel();
+            text2 = new JTextField();
+            text2.setColumns(20);
             panel2.add(new JLabel("Apellido: "));
             panel2.add(text2);
-		
-            JPanel panel3 =new JPanel();
-            String [] civilState = {"Soltero",
+            
+            panel3 =new JPanel();
+            String [] civilState = {"--Seleccionar--",
+            						"Soltero",
                                     "Casado",
                                     "Viudo",
                                     "Divorciado"};
-            JComboBox jComboBox = new JComboBox(civilState);
+            jComboBox = new JComboBox(civilState);
             jComboBox.setSelectedIndex(0);
             panel3.add(new JLabel("Estado Civil: "));
             panel3.add(jComboBox);
 		
-            JPanel panel4 = new JPanel();
-            JTextArea text3 = new JTextArea(10,25);
+            panel4 = new JPanel();
+            text3 = new JTextArea(13,28);
+            text3.setMinimumSize(new Dimension(13,28));
+            text3.setMaximumSize(new Dimension(13,28));
             JScrollPane description = new JScrollPane(text3);
+            description.setMinimumSize(new Dimension(13,28));
+            description.setMaximumSize(new Dimension(13,28));
             panel4.add(new JLabel("Descripcion: "));
             panel4.add(description);
 		
-            JPanel pnlNorte = new JPanel();
+            pnlNorte = new JPanel();
             GridLayout grid0 = new GridLayout (3,1);
             pnlNorte.setLayout(grid0);
             pnlNorte.add(panel1);
             pnlNorte.add(panel2);
             pnlNorte.add(panel3);
-            
+            pnlNorte.setOpaque(false);
 
-            JPanel pnlCentro = new JPanel();
+            pnlCentro = new JPanel();
             pnlCentro.add(panel4);
-            
+            pnlCentro.setOpaque(false);
 		
-            JPanel pnlSur = new JPanel();
+            pnlSur = new JPanel();
             save =new JButton("Guardar");
             save.addActionListener(new MiListener(this));
             pnlSur.add(save);
+            pnlSur.setOpaque(false);
             
             cancel =new JButton("Cancelar");
             cancel.addActionListener(new MiListener(this));
             pnlSur.add(cancel);
             
             
-            JPanel content0 = new JPanel();
+            content0 = new JPanel();
+            content0.setOpaque(false);
             content0.add(pnlNorte, BorderLayout.NORTH);
             content0.add(pnlCentro,BorderLayout.CENTER);
             content0.add(pnlSur, BorderLayout.SOUTH);
+            content0.setVisible(true);
             
-            JPanel textEdit = new JPanel();
-            JTextArea text4 = new JTextArea(17,30);
+            textEdit = new JPanel();
+            text4 = new JTextArea(21,35);
+            text4.setBackground(null);
+            text4.setMinimumSize(new Dimension(21,30));
+            text4.setMaximumSize(new Dimension(21,30));
             JScrollPane edit = new JScrollPane(text4);
-            textEdit.add(edit, BorderLayout.NORTH);
+            edit.setMinimumSize(new Dimension(21,30));
+            edit.setMaximumSize(new Dimension(21,30));
+            textEdit.add(edit, BorderLayout.CENTER);
+            
             negrita = new JButton("B");
+            negrita.addActionListener(new MiListener(this));
+            
             cursiva = new JButton("I");
+            cursiva.addActionListener(new MiListener(this));
+            
             color = new JButton("Color");
-            tamanio = new JButton("Tama�o");
-            tipo = new JButton("Tipo");
+            color.addActionListener(new MiListener(this));
+            
+ 
+            String [] tam = new String[30];
+            for(int i=0;i<tam.length;i++) {
+            	tam[i]=String.valueOf(i);
+            	if(i==0) {
+            		tam[i] = "--Size--";
+            	}
+            }
+				
+            		
+            jComboBoxTam = new JComboBox(tam);
+            jComboBoxTam.setSelectedIndex(0);
+            jComboBoxTam.addActionListener(new MiListener(this));
+            
+ 
+            String [] tipoL = {"--Tipe--",
+            		"Arial Black",
+					"Brush Script MT",
+                    "Calibri Light (Títulos)",
+                    "Cambria",
+                    "Rockwell Extra Bold",
+                    "Times New Roman"};
+            jComboBoxT = new JComboBox(tipoL);
+            jComboBoxT.setSelectedIndex(0);
+            jComboBoxT.addActionListener(new MiListener(this));
+            
             JPanel botones = new JPanel();
             botones.add(negrita);
             botones.add(cursiva);
             botones.add(color);
-            botones.add(tipo);
-            botones.add(tamanio);
+            botones.add(jComboBoxT);
+            botones.add(jComboBoxTam);
             textEdit.add(botones, BorderLayout.SOUTH);
-            /*GridLayout grid1 = new GridLayout (0,1);
-            textEdit.setLayout(grid1);
-            textEdit.add(edit);
-            textEdit.add(botones,BorderLayout.SOUTH);*/
             
             JTabbedPane tabbedPane = new JTabbedPane ();
-            tabbedPane.addTab("Tab 1",content0);
-            tabbedPane.addTab("Tab 2", textEdit);
+            tabbedPane.addTab("Mis Datos",content0);
+            tabbedPane.addTab("Editor de Texto", textEdit);
             
             content1.add(tabbedPane);
+            content1.setVisible(true);
 		
 	}
         public JButton getSave(){
@@ -121,6 +182,54 @@ public class Ventana3 extends JFrame{
         }
         public JButton getCancel(){
             return this.cancel;
+        }
+        public JButton getNegrita(){
+        	return this.negrita;
+        }
+        public JButton getCursiva(){
+        	return this.cursiva;
+        }
+        public JButton getColor(){
+        	return this.color;
+        }
+        public JButton getTamanio(){
+        	return this.tamanio;
+        }
+        public JButton getTipo() {
+        	return this.tipo;
+        }
+        public JTextField getText1() {
+        	return this.text1;
+        }
+        public JTextField getText2() {
+        	return this.text2;
+        }
+        public JTextArea getText3() {
+        	return this.text3;
+        }
+        public JTextArea getText4() {
+        	return this.text4;
+        }
+        public JComboBox getJComboBox() {
+        	return this.jComboBox;
+        }
+        public JComboBox getJComboBoxT() {
+        	return this.jComboBoxT;
+        }
+        public JComboBox getJComboBoxTam() {
+        	return this.jComboBoxTam;
+        }
+        public boolean getValB() {
+    		return this.valB;
+    	}
+        public void setValB(boolean a) {
+        	this.valB=a;
+        }
+        public boolean getValI() {
+    		return this.valI;
+    	}
+        public void setValI(boolean a) {
+        	this.valI=a;
         }
         /*@Override
         public void actionPerformed(ActionEvent e){
